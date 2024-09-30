534fda66d338a7cab0f364196f52e59b5e5cb587
mlhstn <melih.stn98@gmail.com>
2024-09-30 21:20:48 +0300

    new case update

--- 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Customer.java
@@ -1,7 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import com.luv2code.springdemo.mvc.validation.CourseCode;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +10,25 @@ public class Customer {
     @NotNull(message ="is required")
     @Size(min = 1, message ="is required")
     private String lastName = "";
+
+    @NotNull(message ="is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    @CourseCode(value = "TOPS", message = "must start with TOPS")
+    private String courseCode;
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -23,7 +42,23 @@ public class Customer {
         return lastName;
     }
 
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public @NotNull(message = "is required") @Min(value = 0, message = "must be greater than or equal to zero") @Max(value = 10, message = "must be less than or equal to 10") Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(@NotNull(message = "is required") @Min(value = 0, message = "must be greater than or equal to zero") @Max(value = 10, message = "must be less than or equal to 10") Integer freePasses) {
+        this.freePasses = freePasses;
     }
 }
